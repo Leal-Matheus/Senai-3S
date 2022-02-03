3dfbd69757d3ad9d8af71445d2092b6809d751b4
Matheus Leal Palmuti <47026404881@info.scs>
2022-02-03 17:01:42 -0300

    Adiciona projeto completo

---
+++ TextAnalysisYoutube/analyzer.py
@@ -0,0 +1,50 @@
+import pickle, random
+from leia import SentimentIntensityAnalyzer
+from colorama import Fore
+from colorama import Style
+# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+s = SentimentIntensityAnalyzer()
+
+comments = []
+negativos = []
+positivos = []
+
+mais_negativo = ''
+mais_positivo = ''
+max_positivo = -1
+max_negativo = 1
+
+with open('comments', 'rb') as fp:
+    comments = pickle.load(fp)
+
+for comment in comments:
+    score = s.polarity_scores(comment)
+    if score['compound'] >= 0:
+        positivos.append(comment)
+    else:
+        negativos.append(comment)
+
+    if score['compound'] > max_positivo:
+        mais_positivo = comment
+        max_positivo = score['compound']
+
+    if score['compound'] < max_negativo:
+        mais_negativo = comment
+        max_negativo = score['compound']
+
+
+print(f"\n {Fore.GREEN}Comentários positivos: {len(positivos)}{Style.RESET_ALL}.\n")
+print(f"{Fore.BLUE}Comentários negativos: {len(negativos)}{Style.RESET_ALL}")
+
+print(f'\n {Fore.GREEN}Comentário mais positivo:{Style.RESET_ALL}')
+print(f'\n {Fore.GREEN}{mais_positivo}{Style.RESET_ALL}')
+
+print(f'\n {Fore.BLUE}Comentário mais negativo:{Style.RESET_ALL}')
+print(f'\n{Fore.BLUE}{mais_negativo}{Style.RESET_ALL}')
+
+print(f'\n {Fore.BLUE}Comentário negativo do dia :)))){Style.RESET_ALL}')
+print(f'\n{Fore.BLUE}{negativos[random.randint(0, len(negativos))]}{Style.RESET_ALL}')
+
+print(f'\n {Fore.GREEN}Comentário positivo do dia :)))){Style.RESET_ALL}')
+print(f'\n{Fore.GREEN}{positivos[random.randint(0, len(positivos))]}{Style.RESET_ALL}')
